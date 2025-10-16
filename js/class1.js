5bb3f6ebe152503ed2df3cc53f4c966549b466d4
Ch Ihtasham <cihtasham32@gmail.com>
2025-10-16 15:55:04 +0500

    js one

---
+++ js/class1.js
@@ -0,0 +1,24 @@
+console.log('hello world')
+// alert('hello world')
+// let a = prompt('enter your name')
+// console.log(a)
+// confirm('are you 18+')
+
+// var no1 = 1;
+// let no2 = 2;
+// const no3 = 3;
+// console.log(no1, no2, no3)
+
+{
+    var no1 = 1;
+    let no2 = 2;
+    const no3 = 3;
+    console.log('these values belong to the scope bracket', no1, no2, no3)
+}
+const no3 = 3;
+var no1 = 3;
+var no1 = 30;
+let no2 = 6
+no2 = 61
+
+console.log(no1, no2, no3)
