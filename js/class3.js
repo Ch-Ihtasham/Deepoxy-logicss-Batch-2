ebdda04d85fc463313116873ad771ec5c338e54f
Ch Ihtasham <cihtasham32@gmail.com>
2025-10-20 09:06:56 +0500

    class 3

---
+++ js/class3.js
@@ -0,0 +1,35 @@
+// let num1 = Number(prompt('Enter your first Number'))
+// let operator = prompt('Enter Your Operator')
+// let num2 = Number(prompt('Enter your Second Number'))
+// switch (operator) {
+//     case '+':
+//         console.log(num1 + num2)
+//         break;
+//     case '-':
+//         console.log(num1 - num2)
+//         break;
+//     case '*':
+//         console.log(num1 * num2)
+//         break;
+//     case '/':
+//         console.log(num1 / num2)
+//         break;
+//     case '%':
+//         console.log(num1 % num2)
+//         break;
+
+//     default:
+//         console.log('Enter a valid operator')
+//         break;
+// }
+// let no = String(1)
+// console.log(typeof no)
+console.log('hello' + ' world')
+// let a = 10
+// let b = 10
+// b += a
+// console.log(b) 
+
+let str = 'ihtasham'
+console.log(`my name is ${str} im a web developer`)
+
