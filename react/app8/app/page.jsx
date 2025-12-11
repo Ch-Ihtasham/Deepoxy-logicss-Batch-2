06ed4544d21d8d24b70cd430bf9c90348b07d4c5
Ch Ihtasham <cihtasham32@gmail.com>
2025-12-11 13:26:39 +0500

    complete react

--- react/app8/app/page.jsx
+++ react/app8/app/page.jsx
@@ -1,9 +1,17 @@
 // import Image from "next/image";
 "use client"
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { Navbar } from '../components/Navbar'
 export default function Home() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('ali')
+  // function a() {
+  //   console.log('hello world')
+  // }
+  let a = useCallback(() => {
+    console.log('hello world', name)
+  }, [name])
+  const btnColor = useRef()
   // let array = 10_000_000
   // const nums = useMemo(() => {
   //   console.log('rendering')
@@ -25,16 +33,18 @@ export default function Home() {
 
   return (
     <div>
-      <Navbar />
+      <Navbar name={name} fun={a} />
       this is our home page
       <br />
       {/* {JSON.stringify(a)} */}
       {/* {a} */}
 
       <br />
+      <div ref={btnColor}>name</div>
 
       <button className="bg-blue-500 p-2 rounded" onClick={() => {
         setCount(count + 1)
+        btnColor.current.backgroundColor='red'
       }}>Click me{count}</button>
     </div>
   );
