5b40b340a49ab222dddf7d73f868e39c5ef8f978
Ch Ihtasham <cihtasham32@gmail.com>
2025-12-16 13:27:51 +0500

    directory name added

--- backend/class2/server.js
+++ backend/class2/server.js
@@ -1,12 +1,15 @@
 // server.mjs
 import { createServer } from 'node:http';
 import fs from 'fs'
-import path from 'node:path';
+// import path from 'node:path';
+import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url)
-
+// const __dirname = path.dirname(__filename)
+const __dirname = dirname(__filename)
 console.log(__filename)
+console.log(__dirname)
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World!\n');
